49d4c925f50c4dd3f70a845ea416987d0319af34
BugHunter-X <askzenai@gmail.com>
2024-12-12 07:50:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ... other routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  // This component causes the bug
+  return <div>About {/* Missing closing tag */};
+}
+```
